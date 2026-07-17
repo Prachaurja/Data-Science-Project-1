f43e0c22ae986a0b5e3f1b998210953457f9ff82
Prachaurja Sarker <prachaurja@gmail.com>
2026-07-18 05:58:14 +1000

    Practicing Data Type with "typeof()" function.

---
+++ Data Type.R
@@ -0,0 +1,26 @@
+a <- 100
+b <- 20
+print(a)
+print(b)
+print(a+b)
+
+plot(100, 200)
+
+
+69 -> c
+print(c)
+
+b=6
+print(b)
+
+x = 10
+y = 20L
+z = 3i+5
+xy <- "Prachaurja Sarker and Sreya Acharjee"
+yz <- TRUE
+
+print(typeof(x))
+print(typeof(y))
+print(typeof(z))
+print(typeof(xy))
+print(typeof(yz))
